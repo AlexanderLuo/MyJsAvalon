167c1904e610e2134efbe96067e80a8b6402a77c
luohao <496952252@qq.com>
2016-10-17 16:30:56 +0800

    ziji

---
+++ src/filters.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright by Alexrander Luo.
+ * Create Date 2016/10/14
+ * Email 496952252@qq.com
+ */
+avalon.filters.sexFilter = function(str){
+    if(str==1){
+        return '男';
+    }
+    else{
+        return '女';
+    }
+}
